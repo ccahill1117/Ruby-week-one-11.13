6c964c2dd7af050c9af94e27936238f1e418d5de
Cole Marsteller and Christopher Cahill <cole@>
2018-11-13 15:03:03 -0800

    clock angle working

--- clock_angle/spec/clock_angle_spec.rb
+++ clock_angle/spec/clock_angle_spec.rb
@@ -5,6 +5,15 @@ require('pry')
 describe('clock_angle') do
 
   it('returns an hour hand clock angle') do
-    expect(clock_angle("0300")).to(eq(90))
+    expect(clock_angle("03:00")).to(eq(90))
   end
+
+it('returns a minute hand clock angle') do
+  expect(clock_angle("06:35")).to(eq(30))
+end
+
+it('returns the angle difference between the minute hand and the hour hand') do
+  expect(clock_angle("04:45")).to(eq(150))
+end
+
 end
